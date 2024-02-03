89d181fe12dcec894e75e72a71ade3346ef6bcbd
Software-as-Solution <software-as-solution@web.de>
2024-02-03 08:29:22 +0100

    first commit

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,24 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+import React from "react";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[479px] mt-5`}>
+        With our service you can get the perfect credit card for you in just a
+        short time
+      </p>
+      <Button styles="mt-10" />
+    </div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+export default CardDeal;
